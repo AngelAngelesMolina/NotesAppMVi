519d67601622ada9e9ec4ce8d5d25429be3e40a3
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2025-10-13 20:49:03 -0600

    ADD: fakeNoteRepository
    Adding fake repos for testing

--- app/src/androidTest/java/com/jaamcoding/notesapptesting/core/di/TestAppModule.kt
+++ app/src/androidTest/java/com/jaamcoding/notesapptesting/core/di/TestAppModule.kt
@@ -3,6 +3,8 @@ package com.jaamcoding.notesapptesting.core.di
 import android.app.Application
 import androidx.room.Room
 import com.jaamcoding.notesapptesting.core.data.local.NoteDb
+import com.jaamcoding.notesapptesting.core.data.repository.FakeAndroidNoteRepository
+import com.jaamcoding.notesapptesting.core.domain.repository.NoteRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -19,5 +21,12 @@ object TestAppModule {
     fun proviteNotesDb(application: Application): NoteDb {
         return Room.inMemoryDatabaseBuilder(application, NoteDb::class.java).build()
     }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(): NoteRepository {
+        return FakeAndroidNoteRepository()
+    }
+
 
 }
