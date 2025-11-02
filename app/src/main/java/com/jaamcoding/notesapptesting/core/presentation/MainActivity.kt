7f5a44372a746fe23eeb0e859e214936a6dde6c8
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2025-11-01 22:14:19 -0600

    Adding UI NoteListScreen

--- app/src/main/java/com/jaamcoding/notesapptesting/core/presentation/MainActivity.kt
+++ app/src/main/java/com/jaamcoding/notesapptesting/core/presentation/MainActivity.kt
@@ -4,44 +4,52 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.jaamcoding.notesapptesting.core.presentation.ui.theme.NotesAppTestingTheme
+import com.jaamcoding.notesapptesting.note_list.presentation.NoteListScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             NotesAppTestingTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Navigation()
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Navigation(modifier: Modifier = Modifier) {
+    val navController = rememberNavController()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NotesAppTestingTheme {
-        Greeting("Android")
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = Screen.NoteList
+    ) {
+
+        composable<Screen.NoteList> {
+            NoteListScreen(
+                onNavigateToAddNote = {
+
+                    //navController.navigate(Screen.AddNote)
+                }
+            )
+        }
+
+        /*composable<Screen.AddNote> {
+            AddNoteScreen(
+                onSave = {
+                    navController.popBackStack()
+                }
+            )
+        }*/
     }
 }
